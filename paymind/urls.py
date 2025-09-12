718174b92da3f214d34ec3f82a13d3ab7b1add23
v20200315 <v20200315@gmail.com>
2025-09-12 16:29:18 +0800

    feat: Simplify the permissions accounts module, focusing on business.

--- paymind/urls.py
+++ paymind/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("webpages.urls")),
     path("admin/", admin.site.urls),
-    path("api/auth/", include("accounts.urls")),
+    path("api/accounts/", include("accounts.urls")),
     path("api/sandbox/", include("sandbox.urls")),
 ]
